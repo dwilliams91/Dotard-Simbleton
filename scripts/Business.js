f84a69c394f26e0163a737f95bbc6055b32b166d
Your Github User Name <david.lav.williams@gmail.com>
2020-10-15 09:24:37 -0500

    styling

--- scripts/Business.js
+++ scripts/Business.js
@@ -2,8 +2,8 @@ export const businessPartnerHTMLCreator=(myArray)=>{
     return `
     
     <section class=clients 
-    <div class=client__name>${myArray.purchasingAgent.nameFirst} ${myArray.purchasingAgent.nameLast}</div>
-    <div class=client__company> ${myArray.companyName} </div>
+    
+    <div class=client__company> <strong> ${myArray.companyName} </strong></div>
     <div class=client__address-street> ${myArray.addressFullStreet} </div>
     <div class=client__address-city> ${myArray.addressCity}, ${myArray.addressStateCode} ${myArray.addressZipCode}</div>
     <br>
